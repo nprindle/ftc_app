87a5eac3fac9c8d12581933a302350eeaff18f53
Justin Prindle <justindprindle@gmail.com>
2018-01-29 20:17:26 -0500

    Swapped old teleop for new teleop in naming scheme

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OldTeamTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OldTeamTeleop.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -10,31 +11,33 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.MathUtils;
 import org.firstinspires.ftc.teamcode.RobotUtils;
 
-@TeleOp(name = "8573 TeleOp 2", group = "TeleOp")
+// Phases: init, init_loop, start, loop, stop
+@Disabled
+@TeleOp(name = "8573 TeleOp", group = "TeleOp")
+public class OldTeamTeleop extends OpMode {
 
-public class NewTeamTeleop extends OpMode {
     // Keeps track of time since beginning of execution
     private ElapsedTime runtime = new ElapsedTime();
 
     // Wheels
     private DcMotor frontLeft, frontRight,
-            backLeft, backRight, firstFlip;
+            backLeft, backRight,
     // Lift controls the raising/lowering of the grabber arms in order to stack blocks
-
+    lift;
 
     // The arm used in autonomous for hitting the balls
-    //private DcMotor flicker;
-    private Servo flicker, secondFlip;
+    private DcMotor flicker;
+    private Servo   flickerExt;
 
     // The four servos to extend the grabber
-    private CRServo leftFly, rightFly;
+    private CRServo topRight, topLeft, bottomRight, bottomLeft;
     // The arms of the block grabber
     private Servo grabLeft, grabRight;
     // Variable used to cut the speed of the wheels
     // Also used to manage acceleration when driving
     private double scale       = 0;
     // Used to track the position of the block grabbing arms
-    private double grabLeftPos = 0.0, grabRightPos = 0.5, secondFlipPos = 0.0, flickerPos = 0.4;
+    private double grabLeftPos = 0.0, grabRightPos = 0.0;
 
     @Override
     public void loop() {
@@ -48,6 +51,7 @@ public class NewTeamTeleop extends OpMode {
                 left_y_2 = -gamepad2.left_stick_y,
                 right_x_2 = gamepad2.right_stick_x,
                 right_y_2 = -gamepad2.right_stick_y;
+
         // Left trigger will halve the speed
         // Right trigger will double the speed
         // Right bumper will slowly accelerate
@@ -71,79 +75,49 @@ public class NewTeamTeleop extends OpMode {
         backLeft.setPower(vs[2] * scale);
         backRight.setPower(vs[3] * scale);
 
-        if (Math.abs(left_y_2) > 0.1) {
-            telemetry.addLine("Entered the if statement of the first flip");
-            telemetry.addData("First Flip Power:", left_y_2);
-            firstFlip.setPower(left_y_2);
-        } else {
-            firstFlip.setPower(0);
-        }
-
-        if (Math.abs(right_y_2) > 0.1) {
-            if (right_y_2 < 0) {
-                secondFlipPos = MathUtils.constrainServo(secondFlipPos - 0.05);
-            } else if (right_y_2 > 0) {
-                secondFlipPos = MathUtils.constrainServo(secondFlipPos + 0.05);
-            }
-        }
-        secondFlip.setPosition(secondFlipPos);
-        telemetry.addData("Second Flip Pos in loop", secondFlip.getPosition());
-
-        if (gamepad2.left_trigger > 0.1) {
-            leftFly.setPower(1.0);
-            rightFly.setPower(1.0);
-        } else if (gamepad2.right_trigger > 0.1) {
-            leftFly.setPower(-1.0);
-            rightFly.setPower(-1.0);
-        } else {
-            leftFly.setPower(0.0);
-            rightFly.setPower(0.0);
-        }
-
-        // flicker.setPosition(flickerPos);
-        telemetry.addData("Flicker Pos in the loop", flicker.getPosition());
-
-        // LOOK AT THIS FOR VIKAS REQUEST
-        // if(gamepad2.x)
-        // {
-        //     firstFlip.setPower(0.8);
-        //     firstFlip.setPower(0.0);
-        //     grabLeft.setPosition(1.0);
-        //     grabRight.setPosition(1.0);
-        //     firstFlip.setPower(-0.8);
-        // }
-
         // Use the right joystick on gamepad 2 to control the upper lift
-        // if (Math.abs(right_y_2) > 0.1) {
-        //     topRight.setPower(right_y_2);
-        //     bottomLeft.setPower(right_y_2);
-        // } else {
-        //     topRight.setPower(0);
-        //     bottomLeft.setPower(0);
-        // }
+        if (Math.abs(right_y_2) > 0.1) {
+            topRight.setPower(right_y_2);
+            bottomLeft.setPower(right_y_2);
+        } else {
+            topRight.setPower(0);
+            bottomLeft.setPower(0);
+        }
 
         // The left joystick similarly controls the lower lift
-        // if (Math.abs(left_y_2) > 0.1) {
-        //     topLeft.setPower(left_y_2);
-        //     bottomRight.setPower(left_y_2);
-        // } else {
-        //     topLeft.setPower(0);
-        //     bottomRight.setPower(0);
-        // }
+        if (Math.abs(left_y_2) > 0.1) {
+            topLeft.setPower(left_y_2);
+            bottomRight.setPower(left_y_2);
+        } else {
+            topLeft.setPower(0);
+            bottomRight.setPower(0);
+        }
+        // The following would also work:
+        // topLeft.setPower(left_y_2);
+        // bottomRight.setPower(left_y_2);
 
         // 'a' and 'b' of gamepad 2 open and close the block grabbers
         if (gamepad2.a) {
-            grabLeftPos = MathUtils.constrainServo(grabLeftPos - 0.05);
-            grabRightPos = MathUtils.constrainServo(grabRightPos - 0.05);
+            grabLeftPos = MathUtils.constrainServo(grabLeftPos - 0.1);
+            grabRightPos = MathUtils.constrainServo(grabRightPos - 0.1);
         } else if (gamepad2.b) {
-            grabLeftPos = MathUtils.constrainServo(grabLeftPos + 0.05);
-            grabRightPos = MathUtils.constrainServo(grabRightPos + 0.05);
+            grabLeftPos = MathUtils.constrainServo(grabLeftPos + 0.1);
+            grabRightPos = MathUtils.constrainServo(grabRightPos + 0.1);
         }
         grabLeft.setPosition(grabLeftPos);
         grabRight.setPosition(grabRightPos);
-        telemetry.addData("grabLeftPos", grabLeftPos);
-        telemetry.addData("grabRightPos", grabRightPos);
 
+        // The triggers of gamepad 2 will control the lift speed, with
+        // the right trigger raising it and the left lowering it
+        if (gamepad2.right_trigger > 0.1 || gamepad2.left_trigger > 0.1) {
+            lift.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+        } else {
+            lift.setPower(0);
+        }
+
+        // The flicker occasionally falls because it's a DC motor, so
+        // gamepad 1 can lift it back up if necessary
+        flicker.setPower(gamepad1.a ? 0.3 : gamepad1.b ? -0.3 : 0.0);
 
         // Telemetry data to be shown every cycle
         // Float formatting used in an attempt to align the numbers in the
@@ -172,51 +146,39 @@ public class NewTeamTeleop extends OpMode {
         frontRight = RobotUtils.registerMotor(hardwareMap, "right", false, "default");
         backLeft = RobotUtils.registerMotor(hardwareMap, "backLeft", true, "default");
         backRight = RobotUtils.registerMotor(hardwareMap, "backRight", false, "default");
-        firstFlip = RobotUtils.registerMotor(hardwareMap, "firstFlip", false, "default");
+        lift = RobotUtils.registerMotor(hardwareMap, "lift", true, "default");
 
-        //lift = RobotUtils.registerMotor(hardwareMap, "lift", true, "default");
-
-        leftFly = RobotUtils.registerCRServo(hardwareMap, "leftFly", true, 0.0);
-        rightFly = RobotUtils.registerCRServo(hardwareMap, "rightFly", false, 0.0);
-        // bottomRight = RobotUtils.registerCRServo(hardwareMap, "bottomRight", false);
+        topLeft = RobotUtils.registerCRServo(hardwareMap, "topLeft", true);
+        topRight = RobotUtils.registerCRServo(hardwareMap, "topRight", false);
+        bottomLeft = RobotUtils.registerCRServo(hardwareMap, "bottomLeft", true);
+        bottomRight = RobotUtils.registerCRServo(hardwareMap, "bottomRight", false);
 
         grabLeft = RobotUtils.registerServo(hardwareMap, "grabLeft", true, grabLeftPos);
         grabRight = RobotUtils.registerServo(hardwareMap, "grabRight", false, grabRightPos);
 
-        //flicker = RobotUtils.registerMotor(hardwareMap, "flicker", true, "default");
-        flicker = RobotUtils.registerServo(hardwareMap, "flicker", false, flickerPos);
-        secondFlip = RobotUtils.registerServo(hardwareMap, "secondFlip", false, secondFlipPos);
+        flicker = RobotUtils.registerMotor(hardwareMap, "flicker", true, "default");
+        flickerExt = RobotUtils.registerServo(hardwareMap, "flickerExt", true);
 
         telemetry.addData("Status", "Initialization complete");
-        telemetry.addData("Flicker Pos", flicker.getPosition());
-        telemetry.addData("Second Flip Pos", secondFlip.getPosition());
         telemetry.update();
     }
 
     @Override
-    public void init_loop() {}
+    public void init_loop() {
+    }
 
     // simply ensures motors are stopped when start is pressed
     @Override
     public void start() {
-        powerMotors(0, frontLeft, frontRight, backLeft, backRight);
-        // secondFlip.setPosition(0.0);
-        //    grabLeft.setPosition(0.5);
-        // grabRight.setPosition(0.5);
+        powerMotors(0, frontLeft, frontRight, backLeft, backRight, lift);
         runtime.reset();
     }
-
-
-    //  public void delay(double seconds) {
-    //  //   sleep((long) (1000.0 * seconds));
-    //  idle();
-    // }
 
     // ensures that motors are halted when telemetry ends
     // otherwise, would run continually for several seconds
     @Override
     public void stop() {
-        powerMotors(0, frontLeft, frontRight, backLeft, backRight);
+        powerMotors(0, frontLeft, frontRight, backLeft, backRight, lift);
         telemetry.addData("Status", "Stopped");
         telemetry.update();
         runtime.reset();
@@ -227,4 +189,5 @@ public class NewTeamTeleop extends OpMode {
         for (DcMotor motor : motors)
             motor.setPower(power);
     }
+
 }
